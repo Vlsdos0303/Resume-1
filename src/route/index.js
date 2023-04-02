3670e91943d5e331a98190849555a9ffb34031e5
Vladyslav Dihtiar <123164282+Vlsdos0303@users.noreply.github.com>
2023-04-02 12:42:19 +0300

    Revert "upd shopcatalog"

--- src/route/index.js
+++ src/route/index.js
@@ -40,9 +40,7 @@ router.get('/', function (req, res) {
   // res.render генерує нам HTML сторінку
 
   //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('index', {
-    layout: 'basic',
-  })
+  res.render('index', {})
   //                  ↑↑ сюди вводимо JSON дані
 })
 
